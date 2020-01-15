7d8078bdb6a5bbb04fb1ff06f4b3f781c48963a1
Gustavo Castillo <gcdcoder@gmail.com>
2020-01-14 21:38:27 -0600

    Create util functions

---
+++ src/lib/index.js
@@ -0,0 +1,31 @@
+export const trasformCharacter = character => {
+  const {
+    id,
+    name,
+    status,
+    species,
+    created,
+    image,
+    gender,
+    origin,
+    location,
+  } = character
+  return {
+    id,
+    name,
+    image,
+    status,
+    species,
+    gender,
+    created,
+    origin: origin.name,
+    location: location.name,
+  }
+}
+
+export const relativeTime = created => {
+  const createdYear = new Date(created).getFullYear()
+  const currentYear = new Date().getFullYear()
+  const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+  return rtf.format(createdYear - currentYear, 'year')
+}
