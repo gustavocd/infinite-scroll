64bb05be904ec6d1143d7d8e806b75fee3a7565c
Gustavo Castillo <gcdcoder@gmail.com>
2020-01-14 21:38:55 -0600

    Create Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<style>
+  .footer {
+    display: flex;
+    justify-content: center;
+    color: #fff;
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+
+  .footer a {
+    color: inherit;
+    text-decoration: underline;
+  }
+</style>
+
+<footer class="footer">
+  <span>Built with Svelte 3 original design by &nbsp;</span>
+  <a href="https://axelfuhrmann.com/" target="_blank" rel="noopener">
+    Axel Fuhrmann
+  </a>
+</footer>
