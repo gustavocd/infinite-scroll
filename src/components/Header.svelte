3c4e47f3ca3063f535e936309ff328b8ca0b3143
Gustavo Castillo <gcdcoder@gmail.com>
2020-01-14 21:38:47 -0600

    Create Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,34 @@
+<style>
+  .header {
+    height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+  }
+
+  .title {
+    font-size: 3.6em;
+    display: flex;
+    color: var(--text-color);
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .title span {
+    display: block;
+    background-color: var(--text-color);
+    border: medium none;
+    font-size: 1.875rem;
+    color: var(--orange-color);
+    padding: 0 1rem;
+    transform: skew(2deg);
+  }
+</style>
+
+<header class="header">
+  <h1 class="title">
+    The Rick and Morty APP
+    <span>Clone</span>
+  </h1>
+</header>
