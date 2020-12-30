01f9e870c6409dea3d7785ae0245b7d77311cce6
Gustavo Castillo <gcdcoder@gmail.com>
2020-12-30 16:26:29 -0600

    Create fn to fetch Characters from api

--- src/lib/index.js
+++ src/lib/index.js
@@ -29,3 +29,18 @@ export const relativeTime = created => {
   const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
   return rtf.format(createdYear - currentYear, 'year')
 }
+
+export const fetchCharacters = async (context) =>  {
+try {
+    const endpoint = context.characters.length > 0 ? context.pageInfo.next : 'https://rickandmortyapi.com/api/character'
+    const blob = await fetch(endpoint)
+    const { results, info, error } = await blob.json()
+    if (!blob.ok) {
+      return Promise.reject(error)
+    }
+    const characters = results.map(result => transformCharacter(result))
+    return Promise.resolve({ characters, info })
+  } catch (error) {
+    return Promise.reject(error.message)
+  }
+}
